e79a505d31cc61c6e3abc5a94f5f718d736b498d
Artem <af.fozie@gmail.com>
2020-10-24 18:52:26 +0300

    added new files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Features from './components/Features';
+// import Details from './components/Details';
+// import Calendar from './components/Calendar';
+import Footer from './components/Footer';
+import './style.css'
+
+function App() {
+  return (
+    <>
+			<Header/>
+			<Main/>
+			<Features/>
+			<Footer/>
+			
+		</>
+  );
+}
+
+export default App;
