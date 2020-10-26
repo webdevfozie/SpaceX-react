3432244db8aaa9f1ba52a886c9c48f8352f247ec
Artem <af.fozie@gmail.com>
2020-10-26 12:06:50 +0300

    added fetchdata

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import Features from './components/Features'
 // import Details from './components/Details';
 // import Calendar from './components/Calendar';
 import Footer from './components/Footer'
+import FetchData from './service/FetchData'
+
 import './style.css'
 
 class App extends React.Component {
+  fetchData = new FetchData()
+
   state = {
     rocket: 'Falcon 1',
+  }
+
+  componentDidMount() {
+    console.log('this.fetchData: ', this.fetchData)
   }
 
   render() {
